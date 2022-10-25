49ee96bfc4f2e64e7780b4ffc1a5771099196bdf
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-10-25 17:50:47 +0300

    Add files via upload

---
+++ BreakingIntoParts.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, a;
+    cin >> n >> m;
+    a = n / m + 1;
+
+    if (n % m == 0)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << n / m << " ";
+        }
+    }
+    if (n % m != 0)
+    {
+        for (int i = 0; i < m - n % m; i++)
+        {
+            cout << n / m << " ";
+        }
+        for (int i = 0; i < n % m; i++)
+        {
+            cout << a << " ";
+        }
+    }
+    system("pause");
+}
